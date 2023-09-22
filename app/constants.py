a1b64b4943a3bda6a80901b06b63e7dec9596212
ashatz <andrew@greatstrength.me>
2023-09-21 21:28:45 -0700

    Initial app files

---
+++ app/constants.py
@@ -0,0 +1,12 @@
+# Environment
+APP_ENV = 'APP_ENV'
+DEFAULT_APP_ENV = 'prod'
+
+# Configuration file
+CONFIG_FILE_DIRECTORY = 'app/{}'
+APP_SCHEMA_FILE = 'app.yml'
+
+# Configuration
+CONFIGS = 'configs' 
+ENDPOINTS = 'endpoints'
+ERRORS = 'errors'
