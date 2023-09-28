626f831c4a9c1ec02de240a94a39766265dd95a8
ashatz <andrew@greatstrength.me>
2023-09-28 16:11:57 -0700

    Add new reading command

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -2,3 +2,8 @@ from schematics import types as t, Model
 from schematics.transforms import blacklist, whitelist
 
 from ..constants import *
+
+class AddNewReading():
+
+    dimension = t.StringType(default=DIMENSION_8, choices=DIMENSIONS)
+    input = t.ListType(t.IntType(), required=True, min_size=6, max_size=18)
