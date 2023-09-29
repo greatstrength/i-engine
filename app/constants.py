935630d3673c8f5c74d7fcf91fd2b5e5b40f4cc5
ashatz <andrew@greatstrength.me>
2023-09-28 17:00:25 -0700

    Add additional kabbalapp constants

--- app/constants.py
+++ app/constants.py
@@ -17,3 +17,49 @@ DIMENSION_6 = '6'
 DIMENSION_8 = '8'
 DIMENSION_49 = '49'
 DIMENSIONS = [DIMENSION_2, DIMENSION_6, DIMENSION_8, DIMENSION_49]
+
+TWO_D_YARROW_TRANSFORM = {
+    False: 2,
+    True: 3
+}
+
+SIX_D_YARROW_TRANSFORM = {
+    '1': 3,
+    '2': 2,
+    '3': 3,
+    '4': 2,
+    '5': 3,
+    '6': 2
+}
+
+EIGHT_D_YARROW_TRANSFORM = {
+    '1': 3,
+    '2': 2,
+    '3': 3,
+    '4': 2,
+    '5': 3,
+    '6': 2,
+    '7': 3,
+    '8': 2
+} 
+
+FORTY_NINE_D_YARROW_TRANSFORM = {
+    '4': 2,
+    '5': 2,
+    '8': 3,
+    '9': 3
+}
+
+YARROW_SUM_TO_LINES = {
+    6: '--- X ---',
+    7: '---------',
+    8: '---   ---',
+    9: '---( )---'
+}
+
+TRANSFORM = {
+    '2': TWO_D_YARROW_TRANSFORM,
+    '6': SIX_D_YARROW_TRANSFORM,
+    '8': EIGHT_D_YARROW_TRANSFORM,
+    '49': FORTY_NINE_D_YARROW_TRANSFORM
+}
