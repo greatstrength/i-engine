6fce1445aa2e20bebc1cdc814ca31ee7c0006711
ashatz <andrew@greatstrength.me>
2023-09-28 19:51:45 -0700

    Add new command

--- app/interfaces/cli/mappers/command.py
+++ app/interfaces/cli/mappers/command.py
@@ -1,1 +1,7 @@
 from ...commands import *
+
+def add_new_reading(context, request, app_context, **kwargs):
+    return AddNewReading(dict(
+        input = request.input,
+        dimension = request.dimension
+    ))
