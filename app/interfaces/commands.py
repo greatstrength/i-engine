bc046bd9b8f0b6cbb45cab57c03bf90a011a0ed0
ashatz <andrew@greatstrength.me>
2023-09-29 12:21:05 -0700

    Add new name domain event property

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -5,5 +5,6 @@ from ..constants import *
 
 class AddNewReading(Model):
 
+    name = t.StringType(required=True)
     dimension = t.StringType(default=DIMENSION_8, choices=DIMENSIONS)
     input = t.ListType(t.IntType(), required=True, min_size=6, max_size=18)
