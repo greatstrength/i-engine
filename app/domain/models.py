6e8eb0fb83eed9c9c093ad54f86e1d090afb84a7
ashatz <andrew@greatstrength.me>
2023-10-04 23:59:14 -0700

    Wow, it worked...

--- app/domain/models.py
+++ app/domain/models.py
@@ -2,6 +2,10 @@ from ..core.domain import *
 from .constants import *
 
 
+class HexagramLine(ValueObject):
+	pass
+
+
 class Hexagram(ValueObject):
 	name = t.StringType(required=True)
 	secondary_names = t.ListType(t.StringType())
@@ -9,6 +13,7 @@ class Hexagram(ValueObject):
 	yarrow_value = t.StringType(required=True)
 	judgement = t.ListType(t.StringType())
 	image = t.ListType(t.StringType())
+	changing_lines = t.ListType(t.ModelType(HexagramLine))
 
 
 class ResultLine(ValueObject):
@@ -17,7 +22,3 @@ class ResultLine(ValueObject):
 	man_line = t.IntType(required=True)
 	earth_line = t.IntType(required=True)
 	line_value = t.IntType(required=True, choices=[6, 7, 8, 9])
-
-
-class HexagramLine(ValueObject):
-	pass
