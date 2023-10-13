65dee972b45ee62457166616f45fa5b76e833925
ashatz <andrew@greatstrength.me>
2023-10-13 14:47:52 -0700

    Fix last bugs

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -6,5 +6,8 @@ from ..constants import *
 class AddNewReading(Model):
 
     name = t.StringType(required=True)
+    date = t.StringType()
     dimension = t.StringType(default=DIMENSION_8, choices=DIMENSIONS)
     input = t.ListType(t.IntType(), required=True, min_size=6, max_size=18)
+    type = t.StringType(default='general')
+    frequency = t.StringType(default='daily')
