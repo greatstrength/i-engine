9a01314f4e0e61fa0db6649632660331d2bf2bbd
ashatz <andrew@greatstrength.me>
2024-05-11 23:44:56 -0700

    Add Repository Implementation - Yaml Reading Cache

---
+++ app/domain/repositories/reading_cache/yaml.py
@@ -0,0 +1,20 @@
+from . import *
+
+import yaml
+
+class YamlReadingCache(ReadingCache):
+
+    def __init__(self, cache_path: str):
+        self.cache_path = cache_path
+
+    def save(self, reading: ReadingResult):
+        with open(self.cache_path, 'r') as f:
+            readings = yaml.safe_load(f)
+            if readings is None:
+                readings = {}
+            reading_data = reading.to_primitive()
+            key = reading_data.pop('id')
+            readings[key] = reading_data
+
+        with open(self.cache_path, 'w') as f:
+            yaml.dump(readings, f)
