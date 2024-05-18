21829b25ef8140f55f2d0d4c627f16f6142d6c32
ashatz <andrew@greatstrength.me>
2024-05-17 23:34:43 -0700

    Misc - Update Domain General Import

--- app/domain/objects/__init__.py
+++ app/domain/objects/__init__.py
@@ -1,2 +1,6 @@
-from .readings import *
-from .hexagram import *
+from typing import List, Dict
+
+from schematics import types as t, Model
+
+from .readings import ReadingResult, ResultLine
+from .hexagram import Hexagram
