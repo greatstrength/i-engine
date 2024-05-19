10c1bce053896a82fd3e4341dacdef44d1e4aab4
ashatz <andrew@greatstrength.me>
2024-05-19 14:23:18 -0700

    Update get hexagram method to map accordingly.

--- app/domain/repositories/hexagram/yaml.py
+++ app/domain/repositories/hexagram/yaml.py
@@ -6,9 +6,28 @@ class HexagramYamlRepository(HexagramRepository):
 
     def __init__(self, yaml_path: str):
         with open(yaml_path, 'r') as file:
-            hexagrams = yaml.safe_load(file)
-            self.__hexagrams = {k: Hexagram(v, strict=False) for k, v in hexagrams.items()}
+            data = yaml.safe_load(file)
+            self.__data = {k: v for k, v in data.items()}
         
 
     def get(self, number: int) -> Hexagram:
-        return self.__hexagrams.get(number, None)
+        hex_data = self.__data.get(number, None)
+        judgement = '\n'.join(hex_data.get('judgement', []))
+        image = '\n'.join(hex_data.get('image', []))
+        hex = Hexagram({
+            'number': hex_data.get('number', 0),
+            'name': hex_data.get('name', ''),
+            'judgement': judgement,
+            'image': image
+        })
+
+        lines_data = hex_data.get('changing_lines', [])
+        for line_data in lines_data:
+            line = Hexagram.Line({
+                'yarrow_value': line_data.get('yarrow_value', 0),
+                'line_number': line_data.get('line_number', 0)
+            })
+            line.text = '\n'.join(line_data.get('text', []))
+            hex.changing_lines.append(line)
+
+        return hex
