fbb0f4a1c5bc2f23f2d2f2785a876dcf09dc299f
ashatz <andrew@greatstrength.me>
2024-05-19 18:57:35 -0700

    Remove Hexagram Result Object and References

--- app/domain/objects/readings.py
+++ app/domain/objects/readings.py
@@ -11,19 +11,11 @@ class ResultLine(ValueObject):
     line_value = t.IntType(required=True, choices=RESULT_LINE_VALUES)
 
 
-class HexagramResult(ValueObject):
-
-    name = t.StringType(required=True)
-    wilhelm_index = t.IntType(required=True)
-
-
 class ReadingResult(Entity):
 
     name = t.StringType(required=True)
+    date = t.DateType(required=True)
     dimension = t.StringType(required=True, choices=READING_RESULT_DIMENSIONS)
     frequency = t.StringType(default=READING_RESULT_FREQUENCY_DEFAULT, choices=READING_RESULT_FREQUENCIES)
     type = t.StringType(default=READING_RESULT_TYPE_DEFAULT, choices=READING_RESULT_TYPES)
-    date = t.DateType(required=True)
     result_lines = t.ListType(t.ModelType(ResultLine))
-    current_or_previous = t.ModelType(HexagramResult)
-    next = t.ModelType(HexagramResult)
