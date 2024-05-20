96b46b3e979e3384834a88cbe86933a2417be7ba
ashatz <andrew@greatstrength.me>
2024-05-19 18:34:16 -0700

    Update Add New Reading Command with New Constants

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -2,12 +2,13 @@ from schematics import types as t, Model
 from schematics.transforms import blacklist, whitelist
 
 from ..constants import *
+from ..domain import *
 
 class AddNewReading(Model):
 
     name = t.StringType(required=True)
     date = t.StringType()
-    dimension = t.StringType(default=DIMENSION_8, choices=DIMENSIONS)
+    dimension = t.StringType(required=True, choices=READING_RESULT_DIMENSIONS)
     input = t.ListType(t.IntType(), required=True, min_size=6, max_size=18)
-    type = t.StringType(default='general')
-    frequency = t.StringType(default='daily')
+    type = t.StringType(default=READING_RESULT_TYPE_DEFAULT, choices=READING_RESULT_TYPES)
+    frequency = t.StringType(default=READING_RESULT_FREQUENCY_DEFAULT, choices=READING_RESULT_FREQUENCIES)
