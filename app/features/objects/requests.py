95f6a6c0ccbd00ae5ed207b1a0cb0d5eddb89b8f
ashatz <andrew@greatstrength.me>
2024-06-07 16:29:14 -0700

    Feature Requests - Update Input Attribute for both Add New Reading and Add Reading Results

--- app/features/objects/requests.py
+++ app/features/objects/requests.py
@@ -8,8 +8,7 @@ class AddNewReading(Model):
     name = t.StringType(required=True)
     date = t.StringType(serialized_name='reading_date')
     dimension = t.StringType(choices=READING_RESULT_DIMENSIONS)
-    input = t.ListType(t.ListType(t.IntType(), min_size=3, max_size=3), min_size=6, max_size=6, default=[])
-    input_type = t.StringType(choices=['yarrow', 'manual'], default='manual')
+    input = t.StringType(required=False, min_length=18, max_length=36)
     no_input = t.BooleanType(default=False)
     input_type = t.StringType(choices=READING_INPUT_TYPES, default=READING_INPUT_TYPE_STANDARD)
     type = t.StringType(default=READING_RESULT_TYPE_DEFAULT, choices=READING_RESULT_TYPES)
@@ -21,7 +20,7 @@ class AddNewReading(Model):
 
 class AddReadingResults(Model):
     reading_id = t.StringType(required=True)
-    input = t.ListType(t.ListType(t.IntType(), min_size=3, max_size=3), required=True, min_size=6, max_size=6, default=[])
+    input = t.StringType(required=False, min_length=18, max_length=36)
     input_type = t.StringType(choices=READING_INPUT_TYPES, default=READING_INPUT_TYPE_STANDARD)
 
 
