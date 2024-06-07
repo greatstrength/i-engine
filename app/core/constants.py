ed28c684773f68bf9f609f8b0e1818e2c23c8247
ashatz <andrew@greatstrength.me>
2024-06-06 19:00:35 -0700

    Add Default Feature Objects Path Constant

--- app/core/constants.py
+++ app/core/constants.py
@@ -5,3 +5,6 @@ YAML_CACHE_PATH = 'readings.yml'
 
 # Hexagram cache file
 YAML_HEXAGRAM_CACHE_PATH = 'app/hexagrams.yml'
+
+# Default Feature Objects Path
+DEFAULT_FEATURE_OBJECTS_PATH = 'app.features.objects.requests'
