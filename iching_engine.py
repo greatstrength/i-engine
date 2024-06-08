32a81f205c25dde764df231c32fd9eb4ad945dff
ashatz <andrew@greatstrength.me>
2024-06-07 21:47:45 -0700

    Update App Execution.

--- iching_engine.py
+++ iching_engine.py
@@ -3,13 +3,17 @@ from aikicore.config import *
 
 from app import *
 
-command: cli.CliCommandExecution = cli.create_cli_command_execution()
-
-# Preprocess
-os.environ[constants.APP_ENV] = command.env
 
 # Create container.
 container = create_container()
+
+# Get arguments cache.
+args_cache = container.args_cache()
+
+parser = cli.create_cli_parser(args_cache.list())
+
+# Create command.
+command = cli.create_cli_command_execution(parser)
 
 # Get app configuration.
 app_config_reader: AppConfigurationReader = load_app_config_reader(
@@ -17,7 +21,7 @@ app_config_reader: AppConfigurationReader = load_app_config_reader(
 app_config = app_config_reader.load_config(strict=False)
 
 # Create app context.
-app_context = create_app('iching_engine', cli.DEFAULT_INTERFACE, app_config, container, type=cli.CliAppContext)
+app_context = cli.create_app('iching_engine', cli.DEFAULT_INTERFACE, app_config, container, type=cli.CliAppContext)
 
 # Run app context.
 app_context.run(command=command)
