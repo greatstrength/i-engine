6610899a2b60034aa0e29b0070dbd20937ca8325
ashatz <andrew@greatstrength.me>
2025-10-20 18:29:36 -0700

    Models - Settings - Add Settings Module

---
+++ app/models/settings.py
@@ -0,0 +1,174 @@
+"""I Ching Engine Model Settings"""
+
+# *** constants
+
+# ** constant: reading_result_dimensions
+READING_RESULT_DIMENSIONS = [
+    '2', '2.1', '2.2', '6', '8', '49'
+]
+
+# Reading Result Frequencies
+READING_RESULT_FREQUENCIES = [
+    'daily', 'weekly', 'morning', 'afternoon', 'evening', 'timeless'
+]
+READING_RESULT_FREQUENCY_DEFAULT = READING_RESULT_FREQUENCIES[0]
+
+# Reading Result Types
+READING_RESULT_TYPES = [
+    'general', 'elemental', 'cardinal'
+]
+READING_RESULT_TYPE_DEFAULT = READING_RESULT_TYPES[0]
+
+# Reading Input Types
+READING_INPUT_TYPE_STANDARD = 'standard'
+READING_INPUT_TYPE_JSON = 'json'
+READING_INPUT_TYPES = [ 
+    READING_INPUT_TYPE_STANDARD,
+    READING_INPUT_TYPE_JSON
+]
+
+
+# Hexagram Line Yarrow Values
+HEXAGRAM_YARROW_VALUES = [6, 9]
+
+# Hexagram Line Numbers
+HEXAGRAM_LINE_NUMBERS = [1, 2, 3, 4, 5, 6]
+
+# Result Line Values
+RESULT_LINE_VALUES = [
+    6, 7, 8, 9
+]
+
+# Two Dimensional Transform
+TWO_D_TRANSFORM = {
+    False: 2,
+    True: 3
+}
+
+# Two Dimentional (Extended 1) Transform
+TWO_D_EXT_1_TRANSFORM = {
+    1: 2,
+    2: 2,
+    3: 2,
+    4: 3,
+    5: 3,
+    6: 3,
+}
+
+# Two Dimentional (Extended 2) Transform
+TWO_D_EXT_2_TRANSFORM = {
+    1: 2,
+    2: 2,
+    3: 2,
+    4: 2,
+    5: 3,
+    6: 3,
+    7: 3,
+    8: 3
+}
+
+# Six Dimensional Transform
+SIX_D_TRANSFORM = {
+    1: 3,
+    2: 2,
+    3: 3,
+    4: 2,
+    5: 3,
+    6: 2
+}
+
+# Eight Dimensional Transform
+EIGHT_D_TRANSFORM = {
+    1: 3,
+    2: 2,
+    3: 3,
+    4: 2,
+    5: 3,
+    6: 2,
+    7: 3,
+    8: 2
+}
+
+# Forty Nine Dimensional Transform
+FORTY_NINE_D_TRANSFORM = {
+    4: 2,
+    5: 2,
+    8: 3,
+    9: 3
+}
+
+# Yarrow Sum to Printed Lines
+TRANSFORM_SUM_TO_LINES = {
+    6: '--- X ---',
+    7: '---------',
+    8: '---   ---',
+    9: '---( )---'
+}
+
+# Transform Sum to Hexagram Number
+YARROW_TO_HEXAGRAM_NUMBER = {
+    '887787': 55,
+    '878878': 29,
+    '878787': 63,
+    '888877': 19,
+    '888878': 7,
+    '887887': 51,
+    '787878': 64,
+    '787887': 21,
+    '877777': 43,
+    '787778': 50,
+    '787787': 30,
+    '777778': 44,
+    '777878': 6,
+    '778888': 20,
+    '777777': 1,
+    '888787': 36,
+    '788778': 18,
+    '788877': 41,
+    '887878': 40,
+    '878887': 3,
+    '778878': 59,
+    '887778': 32,
+    '887888': 16,
+    '778787': 37,
+    '777787': 13,
+    '877887': 17,
+    '877888': 45,
+    '778778': 57,
+    '788787': 22,
+    '778788': 53,
+    '878888': 8,
+    '778887': 42,
+    '877788': 31,
+    '778877': 61,
+    '777887': 25,
+    '877877': 58,
+    '788788': 52,
+    '878877': 60,
+    '887877': 54,
+    '888788': 15,
+    '878788': 39,
+    '787877': 38,
+    '877878': 47,
+    '888777': 11,
+    '787777': 14,
+    '887777': 34,
+    '877778': 28,
+    '887788': 62,
+    '787888': 35,
+    '788887': 27,
+    '888778': 46,
+    '888888': 2, 
+    '777788': 33, 
+    '888887': 24, 
+    '877787': 49, 
+    '788888': 23, 
+    '777888': 12, 
+    '788777': 26, 
+    '778777': 9, 
+    '777877': 10, 
+    '878777': 5, 
+    '787788': 56, 
+    '878778': 48, 
+    '788878': 4
+}
